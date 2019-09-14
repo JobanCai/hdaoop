c52faaf3767152f13c426d9a415b54e106c75fc9
蔡俊鹏 <Joban_Cai@163.com>
2019-09-14 17:40:32 +0800

    upload file

--- src/main/java/HDFSClient.java
+++ src/main/java/HDFSClient.java
@@ -1,8 +1,11 @@
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
+import org.junit.Test;
 
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 public class HDFSClient {
     public static void main(String[] args) throws IOException {
@@ -12,5 +15,16 @@ public class HDFSClient {
         fs.mkdirs(new Path("caijp"));
         fs.close();
         System.out.println("finish");
+    }
+
+    @Test
+    public void testCopyFromLocal() throws IOException, URISyntaxException, InterruptedException {
+        Configuration configuration = new Configuration();
+        //指定用户名连接hdfs
+        //FileSystem fs = FileSystem.get(new URI("hdfs://127.0.0.1:9000"), configuration, "CJP");
+        FileSystem fs = FileSystem.get(configuration);
+        fs.copyFromLocalFile(new Path("C:\\oem8.log"), new Path("caijp"));
+        fs.close();
+        System.out.println("over");
     }
 }
